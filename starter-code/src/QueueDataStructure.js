4aee5a14be0877224cb2d9a975dd78a159cc0415
William B <william.benn11@gmail.com>
2019-08-18 10:24:46 -0400

    Updated

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,24 @@
-function QueueDataStructure () {
+class QueueDataStructure{
+    constructor(){
+    this.storage ={};
+    this.end =0;
+    this.front =0;
+    }
+
+remove(){
+
+        let results = this.storage[this.front];
+        delete this.storage[this.front];
+        this.front < this.end &&this.front ++;
+        return results;
+    }
+
+end(item){
+    this.storage[this.end] = item;
+    this.end++;
+    
+}
+size(){
+    return this.head - this.tail;
+}
 }
--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,21 @@
-function StackDataStructure () {
-}
+class StackDataStructure{
+    constructor(){
+      this.properties ={};
+      this.size =0;
+    }
+    add(x){
+      this.properties.slice(0,8);
+      this.properties.push(x);
+      this.size++;
+    }
+    pop(){
+      this.size && this.size--;
+      return this.properties.pop();
+      
+    }
+    getSize(){
+      return this.size;
+    }
+  }
+
+  
